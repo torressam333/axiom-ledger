45e7a9eeb6c39e3121cb6d8046ab59a1a88e8b81
torressam333 <torressam333@gmail.com>
2026-03-07 09:49:55 -0800

    test: add failing wallet creation test

---
+++ tests/health_check.rs
@@ -0,0 +1,15 @@
+use axiom_ledger::domain::{Currency, Wallet};
+
+#[test]
+fn wallet_creation_works_for_xrpl() {
+    // Setup part. Using 128 for mathematical precision. No room for error in fintech.
+    let initial_balance: u128 = 100_000_000;
+    let address = "rPT1Sjq2YGrvB3yS2ne8heJWTVyK3u6mcw";
+
+    // Execution part
+    let wallet = Wallet::new(address, initial_balance, Currency::XRP);
+
+    // Verification
+    assert_eq!(wallet.balance, 100_000_000);
+    assert_eq!(wallet.address, address);
+}
