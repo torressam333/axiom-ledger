8e5bcc07f8274156f560ce1e0f3f906fca5778f3
torressam333 <torressam333@gmail.com>
2026-03-08 11:17:28 -0700

    refactor: implement Newtype pattern for Address validation

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,23 +1,38 @@
 #[derive(Debug, PartialEq, Clone)]
+pub struct Address(String);
+
+impl Address {
+    pub fn parse(s: String) -> Result<Self, String> {
+        if !s.starts_with("r") {
+            return Err(format!("{} is not a valid XRPL address", s));
+        }
+
+        Ok(Self(s))
+    }
+
+    pub fn as_str(&self) -> &str {
+        &self.0 // Get inner value
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
 pub enum Currency {
     XRP,
 }
 
 #[derive(Debug)]
 pub struct Wallet {
-    address: String,
+    address: Address,
     balance: u128,
     currency: Currency,
 }
 
 impl Wallet {
-    pub fn new(address: &str, balance: u128, currency: Currency) -> Result<Self, String> {
-        if !address.starts_with("r") {
-            return Err(format!("{} is not a valid XRPL address", address));
-        }
+    pub fn new(addr_str: &str, balance: u128, currency: Currency) -> Result<Self, String> {
+        let address = Address::parse(addr_str.to_string())?;
 
         Ok(Self {
-            address: address.to_string(),
+            address,
             balance,
             currency,
         })
@@ -28,6 +43,6 @@ impl Wallet {
     }
 
     pub fn address(&self) -> &str {
-        &self.address
+        &self.address.as_str()
     }
 }
